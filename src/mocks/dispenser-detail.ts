cad7f17bf3a5921880953ab27b8a33ecabd4c1fb
Maria <maremarismaria@gmail.com>
2024-08-08 20:52:45 +0100

    :clown_face: Add mocks

---
+++ src/mocks/dispenser-detail.ts
@@ -0,0 +1,13 @@
+export const dispenserDetail = [
+    {
+        "amount": 57.678,
+        "usages": [
+            {
+                "opened_at": "2022-01-01T02:00:00Z",
+                "closed_at": "2022-01-01T02:00:50Z",
+                "flow_volume": 0.064,
+                "total_spent": 39.2
+            }
+        ]
+    }
+]
---
+++ src/mocks/dispensers.ts
@@ -0,0 +1,17 @@
+export const dispensers = [
+    {
+        "id": "b3ea6cde-c60d-4c68-b42f-1964205d557f",
+        "status": "open",
+        "updated_at": "2022-01-01T02:00:00Z"
+    },
+    {
+        "id": "b3ea6cde-c60d-4c68-b42f-1964205d557f",
+        "status": "close",
+        "updated_at": "2022-01-01T02:00:00Z"
+    },
+    {
+        "id": "b3ea6cde-c60d-4c68-b42f-1964205d557f",
+        "status": "close",
+        "updated_at": "2022-01-01T02:00:00Z"
+    }
+]
