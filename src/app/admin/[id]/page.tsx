054d8810a234429e8360e1f52b1784e915ee5825
Maria <maremarismaria@gmail.com>
2024-08-11 14:25:25 +0100

    :lock: Protect dispenser detail page

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -3,10 +3,21 @@ import { Loader } from "@/app/components/Loader";
 import { getDispenserDetail } from "@/services/dispensers";
 import { DispenserDetail } from "@/types/dispenser";
 import { useEffect, useState } from "react";
+import { getSession } from "@/services/session";
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
  
 export default function Page({ params }: { params: { id: string } }) {
+    const router = useRouter();
     const [dispenser, setDispenser] = useState({} as DispenserDetail);
+
+    useEffect(() => {
+        const session = getSession();
+  
+        if (!session) {
+            router.push("/");
+        }
+    }, []);
 
     useEffect(() => {
         getDispenserDetail(params.id).then(dispenser => {
@@ -33,7 +44,7 @@ export default function Page({ params }: { params: { id: string } }) {
                         </thead>
                         <tbody>
                             {
-                                dispenser.usages.map(({ opened_at, closed_at, flow_volume, total_spent }) => {
+                                dispenser && dispenser.usages && dispenser.usages.map(({ opened_at, closed_at, flow_volume, total_spent }) => {
                                     return (
                                     <tr key={opened_at}>
                                         <td>{opened_at}</td>
